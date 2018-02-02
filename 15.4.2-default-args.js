b69becc984691f6ab2c4603805d0152e88551ac9
Tomasz Grande <grande.tomasz@gmail.com>
2018-02-03 00:17:15 +0100

    Add exercise 2 solution

---
+++ 15.4.2-default-args.js
@@ -0,0 +1,5 @@
+const multiply = (x=1, y=1) => x*y;
+console.log(multiply(2, 5)); // 2*5=10
+console.log(multiply(6, 6)); // 6*6=36
+console.log(multiply(5)); // 5*1=5
+console.log(multiply()); // 1*1=1
